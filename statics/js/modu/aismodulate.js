f53f14f4746459424e94b27b2192b8c2ac055781
候金成 <houjincheng1992@163.com>
2016-04-25 14:24:32 +0800

    add all

---
+++ statics/js/modu/aismodulate.js
@@ -0,0 +1,15 @@
+/*
+ ais modulate 模拟源入口页面
+ */
+
+var React = require("react");
+
+var aismoulate = {
+    mode: "overview",
+    preloadCondition: {
+
+    },
+    init: function()
+};
+
+module.exports = aismoulate;
